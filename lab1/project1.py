49a427ab5b3cf7168b80c5d2e6b1b12d5f3257af
maejikal <meylinggga@gmail.com>
2025-09-15 22:31:57 +0800

    added comparisons, fix insertionsort code

--- lab1/project1.py
+++ lab1/project1.py
@@ -1,8 +1,12 @@
+global comparisons
+mergesort_comparisons = [0]
 def merge(l1, l2):
+    print(l1, l2)
     ans = []
     p1 = 0
     p2 = 0
     while p1<len(l1) and p2<len(l2):
+        mergesort_comparisons[0] += 1
         if l1[p1] < l2[p2]:
             ans.append(l1[p1])
             p1+=1
@@ -31,11 +35,16 @@ def insertionsort(lst):
             if lst[j]<lst[j-1]:
                 lst[j], lst[j-1] = lst[j-1], lst[j]
                 j-=1
+            else:
+                break
     return (lst, comparisons)
 
 
 def integrate(lst):
     pass
 
-lst = [4, 3]
-print(insertionsort(lst))
+# lst = [5, 4, 2, 1, 3, 9, 10, 15, 12]
+lst = [6, 4, 1, 3, 5, 2]
+# print(insertionsort(lst)[0])
+print(mergesort(lst))
+print(mergesort_comparisons)
