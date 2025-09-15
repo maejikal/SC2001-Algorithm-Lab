1126fe1d4d9e30358c548d2315b1ba9c73212f7c
visrutsuresh <66361851+visrutsuresh@users.noreply.github.com>
2025-09-15 22:24:05 +0800

    Fixed the integrated sort and made it so that we can see the number of comparisons now

--- lab1/project1.py
+++ lab1/project1.py
@@ -1,8 +1,12 @@
+import random
+
 def merge(l1, l2):
     ans = []
     p1 = 0
     p2 = 0
+    comparisons = 0
     while p1<len(l1) and p2<len(l2):
+        comparisons += 1
         if l1[p1] < l2[p2]:
             ans.append(l1[p1])
             p1+=1
@@ -11,7 +15,7 @@ def merge(l1, l2):
             p2+=1
     ans.extend(l1[p1:])
     ans.extend(l2[p2:])
-    return ans
+    return (ans, comparisons)
 
 """def mergesort(lst):
     if len(lst)<=1:
@@ -23,31 +27,42 @@ def merge(l1, l2):
     return merge(mergesort(left), mergesort(right))"""
 
 def insertionsort(lst):
-    a = list(lst)
-    for i in range(1, len(a)):
-        key = a[i]
+    comparisons = 0
+    for i in range(1, len(lst)):
+        key = lst[i]
         j = i - 1
-        while j >= 0 and a[j] > key:
-            a[j + 1] = a[j]
-            j -= 1
-        a[j + 1] = key
-    return a
+        while j >= 0:
+            comparisons += 1
+            if lst[j] > key:
+                lst[j + 1] = lst[j]
+                j -= 1
+            else:
+                break
+        lst[j + 1] = key
+    return (lst, comparisons)
 
 
 def integrate(lst, S=10):
     def hybrid(arr):
         n = len(arr)
         if n <= 1:
-            return list(arr)
+            return (list(arr), 0)
         if n <= S:
             return insertionsort(arr)
         mid = n // 2
-        left = hybrid(arr[:mid])
-        right = hybrid(arr[mid:])
-        return merge(left, right)
+        left_sorted, left_comp = hybrid(arr[:mid])
+        right_sorted, right_comp = hybrid(arr[mid:])
+        merged, merge_comp = merge(left_sorted, right_sorted)
+        return (merged, left_comp + right_comp + merge_comp)
 
     return hybrid(lst)
 
-lst = [4, 3,21,143,14,234,4,12331231,423,25,522,34525,123]
-print(insertionsort(lst))
-print(integrate(lst))
+#lst = [4, 3,21,143,14,234,4,12331231,423,25,522,34525,123]
+lst=[]
+for i in range(10000):
+    lst.append(random.randint(0, 1000000))
+sorted_ins, comps_ins = insertionsort(lst[:])
+sorted_hyb, comps_hyb = integrate(lst)
+print ((comps_ins, comps_hyb))
+#print((sorted_ins, comps_ins))
+#print((sorted_hyb, comps_hyb))
